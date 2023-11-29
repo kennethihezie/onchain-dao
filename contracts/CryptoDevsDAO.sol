129d8afbe1eae5c0df35415f034e06750158f7f0
kennethihezie <kennethihezie06@gmail.com>
2023-11-29 11:52:30 +0100

    formatted code

--- contracts/CryptoDevsDAO.sol
+++ contracts/CryptoDevsDAO.sol
@@ -12,9 +12,9 @@ contract CryptoDevsDAO is Ownable {
      // FakeNFTMarketplace if the proposal passes
      uint256 nftTokenId;
 
-     // deadline - the UNIX timestamp until which this proposal
-     // is active. Proposal can be executed after the deadline has been exceeded.
-     uint256 deadline;
+      // deadline - the UNIX timestamp until which this proposal
+      // is active. Proposal can be executed after the deadline has been exceeded.
+      uint256 deadline;
 
       // yayVotes - number of yay votes for this proposal
       uint256 yayVotes;
@@ -27,7 +27,7 @@ contract CryptoDevsDAO is Ownable {
      bool excuted;
 
      // voters - a mapping of CryptoDevsNFT tokenIDs to booleans indicating
-     //  whether that NFT has already been used to cast a vote or not
+     // whether that NFT has already been used to cast a vote or not
      mapping (uint256 => bool) voters;
   }
 
@@ -134,7 +134,7 @@ contract CryptoDevsDAO is Ownable {
     proposal.excuted = true;
   }
 
-  //The Ownable contract we inherit from, contains a modifier onlyOwner which
+  // The Ownable contract we inherit from, contains a modifier onlyOwner which
   // restricts a function to only be able to be called by the contract owner.
   function withdrawEther() external onlyOwner {
     uint256 amount = address(this).balance;
